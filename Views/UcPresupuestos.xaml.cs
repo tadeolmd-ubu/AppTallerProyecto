cb3a7421337affd0f31bcbe15bad7c48795c2168
tadeolmd-ubu <tadeolmd@gmail.com>
2025-11-11 20:40:51 -0700

    Front para hacer los presupuestos (solo eso por el momento)

---
+++ Views/UcPresupuestos.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppTaller.Views
+{
+    /// <summary>
+    /// Lógica de interacción para UcPresupuestos.xaml
+    /// </summary>
+    public partial class UcPresupuestos : UserControl
+    {
+        public UcPresupuestos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBuscarProducto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lvDetalles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
